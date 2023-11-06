cf8d86c49db7519ca676610e768795c590dc2b47
fabioseixas <dev.fabioseixas@gmail.com>
2023-11-06 08:40:44 -0300

    feat: make it pass again

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -16,7 +16,7 @@ it('get contact list', async () => {
   const result = await request('localhost:3001').get('/contacts');
 
   expect(result.body).toStrictEqual([
-    { name: 'Renata', id: '12345' },
+    { name: 'Renata', id: '1234' },
     { name: 'Lucas', id: '4321' },
   ]);
 });
